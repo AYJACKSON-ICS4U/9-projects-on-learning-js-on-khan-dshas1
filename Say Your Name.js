9e89b3b32b04d3f7fcbd62c6d8a971490756a25b
dshas1 <31663409+dshas1@users.noreply.github.com>
2017-09-12 10:05:56 -0400

    Create Say Your Name.js

---
+++ Say Your Name.js
@@ -0,0 +1,18 @@
+var textX = random(0, 300);
+var textY = random(0, 300);
+var yourName = "Dominic";
+
+var name =function()
+{
+    var textX = random(0, 300);
+var textY = random(0, 300);
+    fill(255, 0, 0);
+    textSize(30);
+    text("Hiiii, " + yourName, textX, textY);
+};
+
+name();
+name();
+name();
+name();
+name();
