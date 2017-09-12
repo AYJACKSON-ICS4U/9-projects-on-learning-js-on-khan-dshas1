4aeff177edae835e6c3eb41e7090b5f4c818bd9d
dshas1 <31663409+dshas1@users.noreply.github.com>
2017-09-12 09:52:30 -0400

    Create Ad Design.js

---
+++ Ad Design.js
@@ -0,0 +1,13 @@
+fill(255, 0, 0);
+textSize(40);
+text("McDonalds", 59, 15,1010,111);
+textSize(15);
+text("I'M LOVIN IT", 129, 81);
+
+fill(237, 180, 23);
+ellipse(168,162,71,9);
+fill(153, 95, 20);
+ellipse(168,153,71,9);
+fill(237, 180, 23);
+ellipse(168,143,71,9);
+
