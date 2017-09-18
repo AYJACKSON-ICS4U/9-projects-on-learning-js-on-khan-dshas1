bb2a1b827dce23cfdc1e8c86457b0e49be7befe3
dshas1 <31663409+dshas1@users.noreply.github.com>
2017-09-18 09:36:30 -0400

    Create Write a Poem.js

---
+++ Write a Poem.js
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+
+<html>
+
+    <head>
+
+        <title>Challenge: Write a Poem</title>
+
+        <meta charset="utf-8">
+
+    </head>
+
+    <body>
+        
+        <h1>Hello World</h1>
+        <p>Roses are red <br> violets are blue<br>.</p> 
+        
+        
+        
+    </body>
+
+</html>
